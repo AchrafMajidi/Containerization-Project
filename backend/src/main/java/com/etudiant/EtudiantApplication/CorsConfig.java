f51732bbcbb2c21ee1ffc06bdc08a8b23c98f833
Achraf <majidiahmed799@gmail.com>
2024-12-22 10:34:55 +0100

    new commit

--- backend/src/main/java/com/etudiant/EtudiantApplication/CorsConfig.java
+++ backend/src/main/java/com/etudiant/EtudiantApplication/CorsConfig.java
@@ -1,12 +1,12 @@
 package com.etudiant.EtudiantApplication;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://frontend.localhost/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 //        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
